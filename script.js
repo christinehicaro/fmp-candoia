bd376084cb7ad5b39e7e8e6e82d82cde7af05e14
Christine Hicaro <christinehicaro@Christines-MacBook-Pro-2.local>
2016-01-24 14:25:16 -0600

    added files

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("hello world");
+
+api.store.put("mySecretKey", 6000);
+
+var results = api.boa.esxec("# How many committers are in each project?" +
+"p: Project = input;" +
+"counts: output sum[string] of int;" +
+
+"committers: map[string] of bool;" +
+
+"visit(p, visitor {" +
+	"before node: Revision ->" +
+		"if (!haskey(committers, node.committer.username)) {" +
+			"committers[node.committer.username] = true;" +
+			"counts[p.id] << 1;" +
+		"}" +
+"});"
+);
